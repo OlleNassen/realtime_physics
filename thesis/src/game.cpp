129a24ad61489c7c7da982fe38ba2286e20e4176
Olle <ollenassen1992@gmail.com>
2019-03-13 19:58:22 +0100

    small changes

--- thesis/src/game.cpp
+++ thesis/src/game.cpp
@@ -88,8 +88,8 @@ game::game()
 	physics_world.player_position.position.z = 50.f;
 	
 	physics_world.player_collider.radius = 1.0f;
-	physics_world.player_collider.weight = 10.f;
-	physics_world.player_collider.elasticity = 0.3f;
+	physics_world.player_collider.weight = 100.f;
+	physics_world.player_collider.elasticity = 0.6f;
 
 	physics_world.enemy_position.old_position.x = 50.f;
 	physics_world.enemy_position.old_position.y = 50.f;
@@ -100,8 +100,8 @@ game::game()
 	physics_world.enemy_position.position.z = 50.f;
 
 	physics_world.enemy_collider.radius = 1.0f;
-	physics_world.enemy_collider.weight = 100.f;
-	physics_world.enemy_collider.elasticity = 0.3f;
+	physics_world.enemy_collider.weight = 10.f;
+	physics_world.enemy_collider.elasticity = 0.6f;
 
 	physics_world.player_position.velocity = glm::vec3(0.0f, 0.0f, 0.0f);
 	physics_world.player_position.acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
